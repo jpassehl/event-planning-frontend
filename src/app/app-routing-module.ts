a5bb9178d09e681d98a141998654ae78b7bfc5e5
Jacqueline Passehl <Jacqueline.Passehl@Slalom.com>
2024-04-30 15:39:18 -0500

    formatting

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -3,8 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { CreateEventIdeaComponent } from './event/create-event-idea/create-event-idea.component';
 
 const routes: Routes = [
-	{path:'create-event-idea', component: CreateEventIdeaComponent }
-]
+	{path:'create-event-idea', component: CreateEventIdeaComponent },
+];
+
+@NgModule({
+	imports: [
+		RouterModule.forRoot(routes, {
+			paramsInheritanceStrategy: 'always',
+		})
+	],
+	exports: [RouterModule],
+	providers: []
+})
 
 export class AppRoutingModule {}
  
