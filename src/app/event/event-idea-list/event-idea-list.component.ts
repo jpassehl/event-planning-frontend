c01458df808f32ded34bb0a5dd90647b38db92dd
jpassehl <JACQUELINEPASSEHL@gmail.com>
2025-04-10 15:06:13 -0500

    formatting and comments

--- src/app/event/event-idea-list/event-idea-list.component.ts
+++ src/app/event/event-idea-list/event-idea-list.component.ts
@@ -4,36 +4,43 @@ import { EventIdeaApiService } from 'src/app/services/event-idea-api.service';
 import { Store } from '@ngrx/store';
 import { EventIdeaState } from 'src/app/state/event-ideas/event-idea.state';
 import * as EventIdeaAction from 'src/app/state/event-ideas/event-idea.action';
-import {selectEventIdeas} from 'src/app/state/event-ideas/event-idea-selector';
+import { selectEventIdeas } from 'src/app/state/event-ideas/event-idea-selector';
 
 @Component({
   selector: 'event-list',
   templateUrl: './event-idea-list.component.html',
-  styleUrls: ['./event-idea-list.component.scss']
+  styleUrls: ['./event-idea-list.component.scss'],
 })
 export class EventListComponent implements OnInit {
   eventList: EventIdea[];
 
-  constructor(private eventIdeaService: EventIdeaApiService, private store: Store<EventIdeaState>){}
+  constructor(
+    private eventIdeaService: EventIdeaApiService,
+    private store: Store<EventIdeaState>
+  ) {}
 
   ngOnInit(): void {
     this.loadEventIdeas();
-  } 
+  }
 
-  handleDeleteEvent(id: string){
+  handleDeleteEvent(id: string) {
     this.eventIdeaService.deleteEventIdea(id).subscribe({
       next: (v) => console.log(v),
       error: (e) => console.error(e),
-      complete: () => this.loadEventIdeas()
-  })
+      complete: () => this.loadEventIdeas(),
+    });
   }
 
-  loadEventIdeas(){
-    this.eventIdeaService.getAllEventIdeas().subscribe((data : EventIdea[]) =>{
-      this.store.dispatch(EventIdeaAction.loadEventIdeasSuccess({
-        eventIdeas: [...data]
-      }));
+  loadEventIdeas() {
+    //Trigger the LoadEventIdeaSucess store action upon sucessful call to API
+    this.eventIdeaService.getAllEventIdeas().subscribe((data: EventIdea[]) => {
+      this.store.dispatch(
+        EventIdeaAction.loadEventIdeasSuccess({
+          eventIdeas: [...data],
+        })
+      );
     });
+    //update the eventList property via the store
     this.store.select(selectEventIdeas).subscribe((eventIdeas) => {
       this.eventList = eventIdeas;
     });
